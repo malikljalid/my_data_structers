67b4fefdf2d1e29429e48f86c7cf35c2de3478fa
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-30 11:44:40 +0100

    main update

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
+#include "myArrays.hpp"
 
 enum enMaxSize { MAXSIZE = 100 };
-
-void insertElemToArray(int elem, int *arr, int &size)
-{
-    arr[size] = elem;
-    size++;
-}
-
-
 
 int main(void)
 {
     int arr[MAXSIZE];
     int size = 0;
 
+    insertElemToArray(7, arr, size);
+    printArrayContent(arr, size);
 
     return (0);
 }
