14f4c3558191109980cc74ad32e4dcd9cc8039c7
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-30 11:30:35 +0100

    insert element to array method

---
+++ arrays/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+enum enMaxSize { MAXSIZE = 100 };
+
+void insertElemToArray(int elem, int *arr, int &size)
+{
+    arr[size] = elem;
+    size++;
+}
+
+
+
+int main(void)
+{
+    int arr[MAXSIZE];
+    int size = 0;
+
+
+    return (0);
+}
