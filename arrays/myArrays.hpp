96c5cc4d2c60a996841bb5173ef9c217af259ea4
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-30 12:48:49 +0100

    add shift right & shift lift algos

--- arrays/myArrays.hpp
+++ arrays/myArrays.hpp
@@ -2,20 +2,41 @@
 #include <iostream>
 
 // ----------- declaration prototypes ----------------
-void insertElemToArray(int elem, int *arr, int &size);
-void printArrayContent(int *arr, int size);
+void arrInsertTop(int elem, int *arr, int &size);
+void arrPrintContent(int *arr, int size);
 
 
 // -------------- inserting methods ----------------
-void insertElemToArray(int elem, int *arr, int &size)
+void arrInsertTop(int elem, int *arr, int &size)
 {
     arr[size] = elem;
     size++;
 }
 
+void arrShiftLift(int *arr, int size)
+{
+    int tmp = arr[0];
+
+    for (int i = 0; i < size - 1; i++)
+        arr[i] = arr[i + 1];
+
+    arr[size - 1] = tmp;
+}
+
+void arrShiftRight(int *arr, int size)
+{
+    int tmp = arr[size - 1];
+
+    for (int i = size - 1; i > 0; i--)
+        arr[i] = arr[i - 1];
+
+    arr[0] = tmp;
+}
+
+
 
 // --------------- printing methods ----------------
-void printArrayContent(int *arr, int size)
+void arrPrintContent(int *arr, int size)
 {
     std::cout << "Array : ";
 
