2317023270ab0674c9d8e26b236ff794a6dddd56
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-30 12:49:09 +0100

    update main

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -8,8 +8,15 @@ int main(void)
     int arr[MAXSIZE];
     int size = 0;
 
-    insertElemToArray(7, arr, size);
-    printArrayContent(arr, size);
+    arrInsertTop(7, arr, size);
+    arrInsertTop(20, arr, size);
+    arrInsertTop(6, arr, size);
+    arrInsertTop(97, arr, size);
+    arrInsertTop(13, arr, size);
+    arrPrintContent(arr, size);
+
+    arrShiftRight(arr, size);
+    arrPrintContent(arr, size);
 
     return (0);
 }
