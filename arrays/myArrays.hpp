53a2d16593f4bb4872d3b7fc59fbad1bf3a682db
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-30 13:17:43 +0100

    quick update on arr print content

--- arrays/myArrays.hpp
+++ arrays/myArrays.hpp
@@ -55,9 +55,7 @@ void arrPrintContent(int *arr, int size)
     std::cout << "Array : ";
 
     for (int i = 0; i < size; i++)
-    {
         std::cout << arr[i] << ' ';
-    }
 
     std::cout << std::endl;
 }
