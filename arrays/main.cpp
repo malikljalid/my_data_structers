d7783aba2bd0d240a49fad551517b87c0e50d9c8
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-10-31 12:13:29 +0100

    update main

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -20,5 +20,30 @@ int main(void)
     arrAddInPosition(42, 2, arr, size);
     arrPrintContent(arr, size);
 
+    arrDeleteTop(arr, size);
+    arrPrintContent(arr, size);
+
+    arrDeleteButtom(arr, size);
+    arrPrintContent(arr, size);
+
+    arrDeletePosition(2, arr, size);
+    arrPrintContent(arr, size);
+
+    arrDeleteValue(13, arr, size);
+    arrPrintContent(arr, size);
+
+    arrAddTop(2001, arr, size);
+    arrAddTop(2001, arr, size);
+    arrAddTop(1999, arr, size);
+    arrAddTop(2001, arr, size);
+    arrAddTop(2100, arr, size);
+    arrPrintContent(arr, size);
+
+    std::cout << "index : " << arrGetIndexOf(2001, arr, size) << std::endl;
+    std::cout << "occ : " << arrGetOccuranceOf(2001, arr, size) << std::endl;
+
+    arrDeleteAllValuesOf(2001, arr, size);
+    arrPrintContent(arr, size);
+
     return (0);
 }
